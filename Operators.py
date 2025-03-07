b67faa2081a6145e3e3b63abb3598e4f9e15252a
Lolla Mohana Lakshmi <150827000+Mohana-Lolla@users.noreply.github.com>
2025-03-07 14:15:49 +0530

    Create Operators.py

---
+++ Operators.py
@@ -0,0 +1,43 @@
+#1.Write a function for arithmetic operators(+,-,*,/)
+def arithmetic_func(a,b):
+    print("Add:",a+b)
+    print(f'subtract:',a-b)
+    print('Multiple:',a*b)
+    print('Divide: ',a/b)
+arithmetic_func(6,2)
+
+#2.Write a method for increment and decrement operators(++, --)
+def inc_dec(num):
+    num=10
+    #incrementing
+    num+=1
+    print("Incremented Value :",num)
+    #decrementing
+    num-=1
+    print("Decremented Value :",num)
+inc_dec(num)
+
+#3Write a program to find the two numbers equal or not
+a = int(input())
+b = int(input())
+if a == b:
+    print("Equal")
+else:
+    print("Not Equal")
+
+#4.Program for relational operators (<,<==, >, >==)
+a = 10
+b = 20
+# Using relational operators
+print("a < b:", a < b)    
+print("a <= b:", a <= b)  
+print("a > b:", a > b)    
+print("a >= b:", a >= b)  
+
+##5.Print the smaller and larger number
+m = int(input())
+n = int(input())
+smaller = min(m,n)
+larger = max(m,n)
+print('Larger number is ', larger)
+print('Smaller number is ', smaller)
